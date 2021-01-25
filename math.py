591d7e0f28c4223bd3768d4bb4e2154214750ca8
Prajakta_Ingle <prajaktaingle09@gmail.com>
2021-01-25 14:59:32 +0530

    math file added

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    if x<y:
+        return ERROR
+    return x-y
+def mul(x,y):
+    return x*y
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    return x/y
+
+def square(x):
+    return x*x
+    
